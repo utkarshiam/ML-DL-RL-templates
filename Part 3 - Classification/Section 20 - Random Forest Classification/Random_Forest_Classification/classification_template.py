b061b197149dbc1e6fb73e9d1253d8f3acb0d216
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-08 19:48:36 +0530

    RF classifier

--- Part 3 - Classification/Section 20 - Random Forest Classification/Random_Forest_Classification/classification_template.py
+++ Part 3 - Classification/Section 20 - Random Forest Classification/Random_Forest_Classification/classification_template.py
@@ -1,4 +1,4 @@
-# Classification template
+# Random Forest Classification
 
 # Importing the libraries
 import numpy as np
@@ -20,11 +20,11 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting classifier to the Training set
-# Create your classifier here
+# Fitting Random Forest Classification to the Training set
 from sklearn.ensemble import RandomForestClassifier
-classifier= RandomForestClassifier(n_estimators=10,criterion='entropy', random_state=0)
-classifier.fit(X_train,y_train)
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
@@ -44,7 +44,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
+plt.title('Random Forest Classification (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -62,7 +62,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
+plt.title('Random Forest Classification (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
