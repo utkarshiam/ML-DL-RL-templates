fbde41e568b1e69b3b0ee9ec7d36ca88fe9e1306
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-14 05:26:53 +0530

    hierarchical clustering

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/Hierarchical_Clustering/data_preprocessing_template.py
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/Hierarchical_Clustering/data_preprocessing_template.py
@@ -6,18 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3,4]].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# 
