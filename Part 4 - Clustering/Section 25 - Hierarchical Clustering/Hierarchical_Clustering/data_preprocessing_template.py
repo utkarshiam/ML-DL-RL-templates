97d594fa91387829a9d1143f9dd044ad3a78ac22
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-14 05:30:05 +0530

    hierarchical clustering

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/Hierarchical_Clustering/data_preprocessing_template.py
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/Hierarchical_Clustering/data_preprocessing_template.py
@@ -9,5 +9,14 @@ import pandas as pd
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3,4]].values
 
+#dendograms to find optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
 
 # 
