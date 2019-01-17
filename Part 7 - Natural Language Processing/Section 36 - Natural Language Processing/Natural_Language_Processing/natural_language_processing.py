f9be69bcd06ea7646b5ce8f9c7557c18c5de3c4c
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-18 05:00:03 +0530

    ANN

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
@@ -45,3 +45,6 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+from sklearn.metrics import accuracy_score
+
+print(accuracy_score(y_test, y_pred))
