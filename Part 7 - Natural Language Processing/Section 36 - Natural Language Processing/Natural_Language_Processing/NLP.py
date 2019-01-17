909e6c4cb180d10ad709d24327d3e66b019de352
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-18 03:10:03 +0530

    Natural Language Processing

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/NLP.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/NLP.py
@@ -2,3 +2,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter='\t', quoting=3)
+
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
