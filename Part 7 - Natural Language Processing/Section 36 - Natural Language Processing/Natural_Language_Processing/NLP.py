4600b0499c6fb4256108de090adc32389cad1ab3
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-18 04:30:25 +0530

    Natural Language Processing-complete

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/NLP.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/NLP.py
@@ -22,8 +22,21 @@ for i in range(0,1000):
     review= ' '.join(review)
     lol.append(review)
 
-    # Creating the Bag of Words model
+# Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(lol).toarray()
 y = dataset.iloc[:, 1].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#ANY ClASSIFIER YOU WANT TO APPLY!!!
+
+
+#ACCURACY
+from sklearn.metrics import accuracy_score
+
+predictions = classifier.predict(X_test)
+
+print(accuracy_score(y_test, predictions))
