525aa86f685b273fe5b008621dd0374e902a61be
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-18 04:25:45 +0530

    Natural Language Processing-bag of words model

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/NLP.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/NLP.py
@@ -12,9 +12,18 @@ import nltk
 nltk.download('popular')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
-review= review.lower()
-review=review.split()
-ps= PorterStemmer()
-review= [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-review= ' '.join(review)
+lol=[]
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review= review.lower()
+    review=review.split()
+    ps= PorterStemmer()
+    review= [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review= ' '.join(review)
+    lol.append(review)
+
+    # Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(lol).toarray()
+y = dataset.iloc[:, 1].values
