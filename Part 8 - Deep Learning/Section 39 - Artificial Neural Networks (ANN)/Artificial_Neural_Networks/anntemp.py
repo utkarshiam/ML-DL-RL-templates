6b096825082677ac661f7aba9ee681caa2886caf
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-27 15:37:41 +0530

    Ann-file

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/anntemp.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/anntemp.py
@@ -5,3 +5,10 @@ import pandas as pd
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+
+#categorical data delete
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
