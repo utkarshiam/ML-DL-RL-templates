4d79ddaca3b1171a7f47578023f4992871c545bd
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-27 19:44:50 +0530

    Ann-file

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/anntemp.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/anntemp.py
@@ -18,3 +18,7 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set can be model_selection
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
