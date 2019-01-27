6fd84002307cf7f303fb9189ab36c8517d4e20da
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-27 19:43:58 +0530

    Ann-file

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/anntemp.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/Artificial_Neural_Networks/anntemp.py
@@ -15,3 +15,6 @@ labelencoder_X_1 = LabelEncoder()
 labelencoder_X_2 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
