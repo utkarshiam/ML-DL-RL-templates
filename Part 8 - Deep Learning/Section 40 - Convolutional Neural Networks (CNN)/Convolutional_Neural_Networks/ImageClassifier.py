d23625e9dcfeeb0d5073ab4378f1c6b82b40a030
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-31 02:10:56 +0530

    CNN Convolution

--- Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/ImageClassifier.py
+++ Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/ImageClassifier.py
@@ -4,3 +4,9 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
