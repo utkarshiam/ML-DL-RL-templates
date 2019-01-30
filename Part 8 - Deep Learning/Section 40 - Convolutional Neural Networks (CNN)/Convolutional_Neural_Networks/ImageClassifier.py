da8db2f48e8113c1fd56a24d0cb290f232b01d6b
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-30 17:32:34 +0530

    CNN initialised

---
+++ Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/ImageClassifier.py
@@ -0,0 +1,6 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
