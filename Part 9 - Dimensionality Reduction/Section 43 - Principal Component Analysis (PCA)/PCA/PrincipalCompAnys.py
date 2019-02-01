991ca2e686537c10839047fe7dc54867b0022b1c
WEIRDLEMON <utkarshdhy@gmail.com>
2019-02-01 17:43:19 +0530

    PCA applied

--- Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/PCA/PrincipalCompAnys.py
+++ Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/PCA/PrincipalCompAnys.py
@@ -15,3 +15,15 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Wine.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
