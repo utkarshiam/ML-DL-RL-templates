14df9c63e78f9d7b1ef8fee7d3f67059adaa1369
WEIRDLEMON <utkarshdhy@gmail.com>
2019-02-01 17:15:53 +0530

    PCA dataet

---
+++ Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/PCA/PrincipalCompAnys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 17:12:39 2019
+
+@author: mudita
+"""
+
+# PCA
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Wine.csv')
