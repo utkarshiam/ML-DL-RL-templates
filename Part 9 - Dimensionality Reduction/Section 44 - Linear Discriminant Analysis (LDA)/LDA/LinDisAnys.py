a20f0140740307007897dee8d6e07e04b6d608c2
WEIRDLEMON <utkarshdhy@gmail.com>
2019-02-05 17:41:29 +0530

    fitting data

--- Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/LDA/LinDisAnys.py
+++ Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/LDA/LinDisAnys.py
@@ -17,7 +17,7 @@ X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -31,3 +31,15 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 lda = LDA(n_components = 2)
 X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+2
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
