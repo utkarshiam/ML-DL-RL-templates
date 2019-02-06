56f0532cd6add1905a83f312ac14af49825b70d7
WEIRDLEMON <utkarshdhy@gmail.com>
2019-02-06 18:03:56 +0530

    k-fold cross validation

--- Part 10 - Model Selection & Boosting/Section 48 - Model Selection/Model_Selection/k_fold_cross_validation.py
+++ Part 10 - Model Selection & Boosting/Section 48 - Model Selection/Model_Selection/k_fold_cross_validation.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -33,7 +33,7 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
 # Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
+from sklearn.cross_validation import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 accuracies.mean()
 accuracies.std()
